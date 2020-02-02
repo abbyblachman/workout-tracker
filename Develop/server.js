9ced1914ee7237c93bf072a74eab29ed7647db7d
Abby <abbysblachman@gmail.com>
2020-02-02 13:43:00 -0600

    adding seeds to server file

--- Develop/server.js
+++ Develop/server.js
@@ -3,14 +3,21 @@ const logger = require("morgan");
 const mongoose = require("mongoose");
 const PORT = 8080;
 const app = express();
+const seeders = require('./seeders/seed');
+
+
 app.use(logger("dev"));
+
+
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 app.use(express.static("public"));
-mongoose.connect("mongodb://localhost/workout", {
-  useNewUrlParser: true,
-  useFindAndModify: false
+
+mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost/workout', {
+  useNewUrlParser: true
 });
+
+
 // routes
 app.use(require("./routes/api.js"));
 app.use(require("./routes/html.js"));
