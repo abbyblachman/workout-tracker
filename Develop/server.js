80635dc1ef746243b4b13ba8317170aba32052e6
Abby <abbysblachman@gmail.com>
2020-02-02 13:59:08 -0600

    mlab

---
+++ Develop/server.js
@@ -0,0 +1,26 @@
+const express = require("express");
+const logger = require("morgan");
+const mongoose = require("mongoose");
+const PORT = 8080;
+const app = express();
+const seeders = require('./seeders/seed');
+
+
+app.use(logger("dev"));
+
+
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+app.use(express.static("public"));
+
+mongoose.connect(process.env.MONGODB_URI || 'mongodb://<dbuser>:<dbpassword>@ds033069.mlab.com:33069/heroku_28hk2qcc', {
+  useNewUrlParser: true
+});
+
+
+// routes
+app.use(require("./routes/api.js"));
+app.use(require("./routes/html.js"));
+app.listen(PORT, () => {
+  console.log(`App running on port ${PORT}!`);
+});
